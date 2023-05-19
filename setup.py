f64e153bd9b88ffa4368179abebe65576c216138
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-19 16:00:52 -0700

    add packaging script

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+def get_requirements():
+    with open('requirements.txt') as f:
+        return f.read().strip().splitlines()
+
+
+setup(
+    name="chainy",
+    version="0.1.0-dev1",
+    description="Declarative prompt chaining",
+    author="Lucas Lofaro",
+    author_email="lucasmlofaro@gmail.com",
+    url="https://github.com/FyZyX/chainy",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=get_requirements(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.9",
+)
