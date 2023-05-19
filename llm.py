f5ee5cebc19f8eb7c4990413fe17ad573cd6d47b
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-19 14:54:24 -0700

    reduce waiting time for mock LLM execution

--- llm.py
+++ llm.py
@@ -16,12 +16,12 @@ class ChatModelProtocol(Protocol):
 
 class MockLanguageModel:
     async def generate(self, prompt: str) -> str:
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.5)
         return f"Mock prompt response for {prompt}"
 
 
 class MockChatModel:
     async def generate(self, messages: ChatMessages) -> str:
         result = "\n".join((f"{m['system']}: {m['content']}" for m in messages))
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.5)
         return f"Mock prompt response for {result}"
